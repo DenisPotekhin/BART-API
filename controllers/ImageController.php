417cfd45155624d20abdb9075492d03862561ff1
Denis <potekhin@umgoroda.ru>
2019-12-12 17:24:20 +0100

    Working version 1.0

--- controllers/ImageController.php
+++ controllers/ImageController.php
@@ -3,6 +3,7 @@
 
 namespace app\controllers;
 
+use app\models\Gallery;
 use Yii;
 use yii\rest\ActiveController;
 use yii\imagine\Image;
@@ -15,31 +16,30 @@ class ImageController extends ActiveController
     public function actions()
     {
         $actions = parent::actions();
-
         // disable all default actions
         unset($actions['index'], $actions['create'], $actions['view'], $actions['update'], $actions['delete']);
-
         return $actions;
     }
 
     public function resizeFile($width, $height, $fileName)
     {
-
-        return Image::resize($fileName, $width, $height);
+        $size = getimagesize($fileName);
+        $imageWidth = ($width == 0) ? $size[0] : $width;
+        $imageHeight = ($height == 0) ? $size[1] : $height;
+        return Image::resize($fileName, $imageWidth, $imageHeight);
     }
 
     public function actionImageFile($width, $height, $path, $name)
     {
+        $modelClass = $this->modelClass;
         $fileName = fileEnCode($path , $name);
         $pathSave = Yii::getAlias('@webroot') . Yii::$app->params['uploadsPath'];
         $filePath = $pathSave . $fileName;
-    //    $img = Imagick::open($filePath);
-    //    $img->resize($width, $height);
-    //    return $this->resizeFile($width, $height, $filePath);
-        if (file_exists($filePath)) {
+        $galleryId = Gallery::findOne(['path' => $path]);
+        $image = $modelClass::findOne(['path' => $name, 'gallery_id' => $galleryId->id]);
+        if (file_exists($filePath) && $image) {
             $fileResize = $pathSave . 'resize' . $fileName;
             Yii::$app->response->statusCode = 200;
-    //        $this->resizeFile($width, $height, $filePath)->save($fileResize);
             return Yii::$app->response
                 ->sendFile($this->resizeFile($width, $height, $filePath)->show('jpg'));
         } else {
